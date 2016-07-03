e5ae37b17f9e816a5e922581ab04816c42b5e43e
Daniel Haaser <thedapperdan@gmail.com>
2016-07-02 21:15:48 -0400

    zip code --> post code

--- API-Sandbox/Challenges.swift
+++ API-Sandbox/Challenges.swift
@@ -35,7 +35,7 @@ internal func exerciseOne() {
      
      Now it's your turn to get the rest of the values needed to print the following:
      
-     "<first name> <last name> lives at <street name> in <city>, <state>, <zip code>. 
+     "<first name> <last name> lives at <street name> in <city>, <state>, <post code>. 
      If you want to contact <title>. <last name>, you can email <email address> or 
      call at <cell phone number>."
      
